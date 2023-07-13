b69dd0448b4ec89b9446fd1b39c598fe2ed0923d
rohan-ap <103110840+rohan-ap@users.noreply.github.com>
2023-07-13 15:11:12 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,7 @@ application = Flask(__name__)
 @application.route('/')
 @application.route('/status')
 def status():
-    return jsonify({'status': 'ok'})
+    return jsonify({'stat': 'ok'})
 
 
 @application.route('/prediction', methods=['POST'])
